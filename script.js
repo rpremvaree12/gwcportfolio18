66211ba6ceacde2edb31a9d3b822fd49a15908e0
Richard Premvaree <rpremvaree12@users.noreply.github.com>
2018-07-19 09:47:32 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function popup(){
+  alert("Hello Richard!");
+
+}
+
+
+function happyDog(){
+  document.getElementById('dog').src='https://i.imgflip.com/4a8he.jpg';
+}
+
+function sadDog(){
+  document.getElementById('dog').src='http://www.yourpetsbestfriend.com/.a/6a00d8341bfe0853ef0133f5132aba970b-800wi';
+}
